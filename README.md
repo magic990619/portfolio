429fb6ca99c37b1ff6e72b632afaa856785688a4
magic990619 <magicrjh0619@gmail.com>
2022-07-01 13:39:17 -0500

    Initialize

---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center"> 
+    <img src="images/masterportfolio-banner-light.png" align="center" height="150"></img>
+</p>
+
+<h1 align="center"> Software Developer Master Portfolio 🔥 </h1> 
+<h3 align="center"> A clean, beautiful, responsive, and 100% customizable portfolio <br /> </h3>
+
+# Clone and Use 📋
+
+- The website is completely built on `react-js` library of `javascript` and that's why we need `nodejs` and `npm` installed
+- While installing `nodejs` and `npm`, try to install versions which are equal or greater than the versions mentioned in badges above
+- In case you want to help developing it or simply saving it, you can fork the repository just by clicking the button on the top-right corner of this page
+- After the successful installation of `nodejs` and `npm`, clone the repository into your local system using below command:
+    ```bash
+     git clone https://github.com/ashutosh1919/masterPortfolio.git
+    ```
+    This will clone the whole repository in your system.
+- To download required dependencies to your system, navigate to the directory where the cloned repository resides and execute following command:
+    ```node
+    npm install
+    ```
+- Now the project is ready to use
+- You can check it using `npm start`, it will open the website locally on your browser.
+# License 📄
+
+This project is licensed under the MIT License - see the [LICENSE.md](./LICENSE) file for details.
---
+++ src/portfolio.js
@@ -0,0 +1,564 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Blake's Portfolio",
+  description:
+    "Hands-on, successful Software Engineer with decades of veriﬁable success leading teams in delivering appropriate technology solutions for Web2 and Web3 products.",
+  og: {
+    title: "Blake Lin Portfolio",
+    type: "website",
+    url: "http://magic990619.github.io/",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Blake Lin",
+  logo_name: "BlakeLin",
+  nickname: "magic-dev",
+  subTitle:
+    "Hands-on, successful Software Engineer with decades of veriﬁable success leading teams in delivering appropriate technology solutions for Web2 and Web3 products.",
+  portfolio_repository: "https://github.com/magic990619",
+  githubProfile: "https://github.com/magic990619",
+};
+
+const socialMediaLinks = [
+  {
+    name: "Github",
+    link: "https://github.com/magic990619",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "Gmail",
+    link: "mailto:blakelin1028@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  // {
+  //   name: "LinkedIn",
+  //   link: "",
+  //   fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+  //   backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  // },
+  {
+    name: "YouTube",
+    link: "",
+    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
+    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
+  },
+  {
+    name: "Twitter",
+    link: "",
+    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  },
+  {
+    name: "Facebook",
+    link: "",
+    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
+    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+  },
+  // {
+  //   name: "Instagram",
+  //   link: "",
+  //   fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+  //   backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  // },
+];
+
+const skills = {
+  data: [
+    {
+      title: "Blockchain & AI",
+      fileName: "DataScienceImg",
+      skills: [
+        "⚡ Designing and building smart contracts architecture, incentives structure, yield pools and strategies using solidity.",
+        "⚡ Experience of working with DeFi & NFT.",
+        "⚡ Complex quantitative modelling for dynamic forecasting and time series analysis",
+      ],
+      softwareSkills: [
+        // {
+        //   skillName: "Tensorflow",
+        //   fontAwesomeClassname: "logos-tensorflow",
+        //   style: {
+        //     backgroundColor: "transparent",
+        //   },
+        // },
+        // {
+        //   skillName: "Keras",
+        //   fontAwesomeClassname: "simple-icons:keras",
+        //   style: {
+        //     backgroundColor: "white",
+        //     color: "#D00000",
+        //   },
+        // },
+        // {
+        //   skillName: "PyTorch",
+        //   fontAwesomeClassname: "logos-pytorch",
+        //   style: {
+        //     backgroundColor: "transparent",
+        //   },
+        // },
+        // {
+        //   skillName: "Python",
+        //   fontAwesomeClassname: "ion-logo-python",
+        //   style: {
+        //     backgroundColor: "transparent",
+        //     color: "#3776AB",
+        //   },
+        // },
+      ],
+    },
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using React-Redux",
+        "⚡ Developing mobile applications using Flutter, React Native and solo android apps using Kotlin",
+        "⚡ Creating application backend in Node, Express & Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Sass",
+          fontAwesomeClassname: "simple-icons:sass",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Yarn",
+          fontAwesomeClassname: "simple-icons:yarn",
+          style: {
+            color: "#2C8EBB",
+          },
+        },
+        {
+          skillName: "Gatsby",
+          fontAwesomeClassname: "simple-icons:gatsby",
+          style: {
+            color: "#663399",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases",
+        "⚡ Deploying deep learning models on cloud to use on mobile devices",
+        "⚡ Setting up streaming jobs from DB to Server or vice-versa on GCP and AWS",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Azure",
+          fontAwesomeClassname: "simple-icons:microsoftazure",
+          style: {
+            color: "#0089D6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "Kubernetes",
+          fontAwesomeClassname: "simple-icons:kubernetes",
+          style: {
+            color: "#326CE5",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "HackerRank",
+      iconifyClassname: "simple-icons:hackerrank",
+      style: {
+        color: "#2EC866",
+      },
+      profileLink: "https://www.hackerrank.com/layman_brother",
+    },
+    {
+      siteName: "Codechef",
+      iconifyClassname: "simple-icons:codechef",
+      style: {
+        color: "#5B4638",
+      },
+      profileLink: "https://www.codechef.com/users/ashutosh_1919",
+    },
+    {
+      siteName: "Codeforces",
+      iconifyClassname: "simple-icons:codeforces",
+      style: {
+        color: "#1F8ACB",
+      },
+      profileLink: "http://codeforces.com/profile/layman_brother",
+    },
+    {
+      siteName: "Hackerearth",
+      iconifyClassname: "simple-icons:hackerearth",
+      style: {
+        color: "#323754",
+      },
+      profileLink: "https://www.hackerearth.com/@ashutosh391",
+    },
+    {
+      siteName: "Kaggle",
+      iconifyClassname: "simple-icons:kaggle",
+      style: {
+        color: "#20BEFF",
+      },
+      profileLink: "https://www.kaggle.com/laymanbrother",
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "National University of Singapore",
+      subtitle: "B.Tech. in Computer Science",
+      logo_path: "nsu_logo.png",
+      alt_name: "NSU",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, CryptoGraphy, Cloud Computing and Full Stack Development.",
+      ],
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Machine Learning",
+      subtitle: "- Andrew Ng",
+      logo_path: "stanford_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "Stanford University",
+      color_code: "#8C151599",
+    },
+    {
+      title: "Deep Learning",
+      subtitle: "- Andrew Ng",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
+      alt_name: "deeplearning.ai",
+      color_code: "#00000099",
+    },
+    {
+      title: "ML on GCP",
+      subtitle: "- GCP Training",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/EB4VJARK8647",
+      alt_name: "Google",
+      color_code: "#0C9D5899",
+    },
+    {
+      title: "Data Science",
+      subtitle: "- Alex Aklson",
+      logo_path: "ibm_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/PLEAPCSJBZT5",
+      alt_name: "IBM",
+      color_code: "#1F70C199",
+    },
+    {
+      title: "Big Data",
+      subtitle: "- Kim Akers",
+      logo_path: "microsoft_logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/164zKCFOsI4vGqokc-Qj-e_D00kLDHIrG/view",
+      alt_name: "Microsoft",
+      color_code: "#D83B0199",
+    },
+    {
+      title: "Advanced Data Science",
+      subtitle: "- Romeo Kienzler",
+      logo_path: "ibm_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/BH2T9BRU87BH",
+      alt_name: "IBM",
+      color_code: "#1F70C199",
+    },
+    {
+      title: "Advanced ML on GCP",
+      subtitle: "- GCP Training",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/5JZZM7TNQ2AV",
+      alt_name: "Google",
+      color_code: "#0C9D5899",
+    },
+    {
+      title: "DL on Tensorflow",
+      subtitle: "- Laurence Moroney",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/6T4DCUGNK8J8",
+      alt_name: "deeplearning.ai",
+      color_code: "#00000099",
+    },
+    {
+      title: "Fullstack Development",
+      subtitle: "- Jogesh Muppala",
+      logo_path: "coursera_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/certificate/NRANJA66Y2YA",
+      alt_name: "Coursera",
+      color_code: "#2A73CC",
+    },
+    {
+      title: "Kuberenetes on GCP",
+      subtitle: "- Qwiklabs",
+      logo_path: "gcp_logo.png",
+      certificate_link:
+        "https://google.qwiklabs.com/public_profiles/e4d5a92b-faf6-4679-a70b-a9047c0cd750",
+      alt_name: "GCP",
+      color_code: "#4285F499",
+    },
+    {
+      title: "Cryptography",
+      subtitle: "- Saurabh Mukhopadhyay",
+      logo_path: "nptel_logo.png",
+      certificate_link:
+        "https://drive.google.com/open?id=1z5ExD_QJVdU0slLkp8CBqSF3-C3g-ro_",
+      alt_name: "NPTEL",
+      color_code: "#FFBB0099",
+    },
+    {
+      title: "Cloud Architecture",
+      subtitle: "- Qwiklabs",
+      logo_path: "gcp_logo.png",
+      certificate_link:
+        "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
+      alt_name: "GCP",
+      color_code: "#4285F499",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  // subtitle: "Work, Internship and Volunteership",
+  description:
+    "I have worked with many evolving startups as Senior Full-Stack Developer and Software Architect. I have also worked with some well established companies mostly as BlockChain Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      experiences: [
+        {
+          title: "Project Lead Developer",
+          company: "yearn.finance",
+          company_url: "https://yearn.finance/",
+          logo_path: "yearn.jpg",
+          duration: "Feb 2022 - June 2022",
+          location: "Remote, US",
+          description:
+            "I lead development of YWatch team as well as prioritize, plan and coordinate project development activities according to strategists requirements.",
+          color: "#0879bf",
+        },
+        {
+          title: "Senior Smart Contract Developer",
+          company: "Custos.Finance",
+          company_url: "",
+          logo_path: "custos.jfif",
+          duration: "May 2021 - Dec 2021",
+          location: "Remote, US",
+          description:
+            "I Design and build a smart contracts architecture, incentives structure, yield pools and strategies using solidity. I design, implement and test smart contract upgrades and additions. I work with the community and smart contract auditors to implement ﬁxes.",
+          color: "#9b1578",
+        },
+        {
+          title: "Senior Frontend Engineer",
+          company: "Yam.Finance",
+          company_url: "https://yam.finance",
+          logo_path: "yam.png",
+          duration: "June 2021 - Dec 2021",
+          location: "Remote, US",
+          description:
+            "Implemented complex UIs using React + Redux following Figma Designs. Developed well-animated websites in Staking, Degenerative, and Governance projects.",
+          color: "#fc1f20",
+        },
+        {
+          title: "Senior Blockchain Developer",
+          company: "Simublade",
+          company_url: "https://simublade.com/",
+          logo_path: "simublade.jfif",
+          duration: "Dec 2020 - Jun 2021",
+          location: "Houston, TX",
+          description:
+            "Involved in enterprise-wide R&D and POC projects, including CI/CD adoption, PAAS cloud migration, version control system modernization, Blockchain technologies evaluation. Developed and enhanced the React.js, HTML5, and CSS3 templates to standardize the development of mobile apps for customers.",
+          color: "#0879bf",
+        },
+        {
+          title: "Blockchain Developer",
+          company: "Unicsoft",
+          company_url: "https://unicsoft.com/",
+          logo_path: "unicsoft.png",
+          duration: "Jan 2017 - Oct 2020",
+          location: "Singapore",
+          description:
+            "Worked as the main developer in several big projects. Served as an innovative management consultant & strategic leader of Internet of Things, Big Data, Analytics, & blockchain.",
+          color: "#9b1578",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
+  avatar_image_path: "projects_image.svg",
+};
+
+const publicationsHeader = {
+  title: "Publications",
+  description:
+    "I have worked on and published a few research papers and publications of my own.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "avatar.png",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with Blockchain, AI, React, Android, Cloud and Opensource Development.",
+  },
+  addressSection: {
+    title: "Email",
+    subtitle:
+      "blakelin1028@gmail.com",
+  },
+  phoneSection: {
+    title: "Phone Number",
+    subtitle: "+1 662 612 0893",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  publicationsHeader,
+  contactPageData,
+};
